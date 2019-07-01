ada519ba5dceafdd6e51d95a575a18e02bc45e1e
BRUNO PEREIRA DE ALMEIDA <bruno.hung@gmail.com>
2019-07-01 15:51:19 -0300

    Update deduplicate.sql

--- sqlserver/deduplicate.sql
+++ sqlserver/deduplicate.sql
@@ -3,3 +3,14 @@ SELECT * FROM (
 	Rank () OVER (PARTITION BY id_column ORDER BY column1 DESC, column2) AS [Rank]
 	FROM table
 ) X WHERE Rank=1
+
+
+------------------------------------------------------------------------
+--alternative
+WITH cte AS (
+  SELECT PROCOD, TESCOD, DATACOD, 
+     row_number() OVER(PARTITION BY PROCOD, TESCOD, DATACOD ORDER BY DATACOD) AS [rn]
+  FROM DW_ESTOQUE_RESUMO
+)
+
+DELETE cte WHERE [rn] > 1
